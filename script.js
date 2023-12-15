1c8297c0090b466974779563af5fb56d1fe107e3
Jean-François Cartier <142059735+jfcmontmorency@users.noreply.github.com>
2023-12-14 23:44:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// ⬇️ Au chargement de la page, lorsqu'on clique sur le bouton "Commencer", il faut faire disparaitre l'élément avec l'identifant  "buttons". Il faut également faire jouer la piste sonore trame.mp3 en boucle
+
+// ⬇️ Activez ici l'animation parallax qui donne l'effet que le personnage avance.
