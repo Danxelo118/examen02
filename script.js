9477016dd9fd1e736245d965777887326cb7e932
Danxelo118 <93773597+Danxelo118@users.noreply.github.com>
2023-12-19 14:19:12 -0500

    ss

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const controlsBouton = document.querySelector('#controls button');
+    const controls = document.getElementById('controls');
+    const audio = new Audio('audio/trame.mp3');
+    const character = document.getElementById('character');
+    const parallaxContainer = document.querySelector('.parallax-container');
+
+    function commencer() {
+        controls.style.display = 'none';
+        controlsBouton.style.display = 'none';
+        audio.loop = true;
+        audio.play();
+
+    character.classList.add('running');
+
+    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener('keyup', handleKeyRelease);
+    }
+    function handleKeyPress (event) {
+        if (event.key === 'D' || event.key === 'd') {
+            character.classList.add('running');
+            parallaxContainer.classList.add('running');
+        }
+    };
+
+    function handleKeyRelease (event) {
+        if (event.key === 'D' || event.key === 'd') {
+            character.classList.remove('running');
+            parallaxContainer.classList.remove('running');
+        }
+    }
+});
+
+
+
+
+
 
