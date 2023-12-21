186e86293b06529477ad5f767927b340897ed4ff
Danxelo118 <danieldezemma@gmail.com>
2023-12-21 14:09:28 -0500

    format

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     const character = document.getElementById('character');
     const audio = new Audio('audio/trame.mp3');
     const controls = document.getElementById('controls');
@@ -6,45 +6,45 @@
     const parallaxContainer = document.querySelector('.parallax-container');
 
     document.querySelector('#controls button').addEventListener('click', startBouton);
-    function startBouton(){
-    controls.style.display = 'none';
-    audio.loop = true;
-    audio.play();
-    startAnimation();
-  };
-    
-  function startAnimation() {
-    
-    character.style.backgroundImage = 'url("images/personnage/Idle.png")';
-    character.classList.add('IdleAnimation');
-}
+    function startBouton() {
+        controls.style.display = 'none';
+        audio.loop = true;
+        audio.play();
+        startAnimation();
+    };
 
+    function startAnimation() {
 
-  document.addEventListener('keydown', startD);   
-  function startD(event) {
-    if (event.key === 'D' || event.key === 'd') {
-        character.style.backgroundImage = 'url("images/personnage/Run.png")';
-        character.classList.add('runAnimation');
-        parallaxContainer.classList.add('parallaxAnimation');
-        parallaxImages.forEach((image) => {
-        image.classList.add('parallaxAnimation');
-    });
-    }
-  }
-
-  document.addEventListener('keyup', stopD);
-  function stopD(event) {
-    if (event.key === 'D' || event.key === 'd') {
-        character.style.backgroundImage = 'url("images/personnage/Run.png")';
-        character.classList.remove('runAnimation');
         character.style.backgroundImage = 'url("images/personnage/Idle.png")';
         character.classList.add('IdleAnimation');
-        parallaxContainer.classList.remove('parallaxAnimation');
-        parallaxImages.forEach((image) => {
-        image.classList.remove('parallaxAnimation');
-  });
     }
-     }
+
+
+    document.addEventListener('keydown', startD);
+    function startD(event) {
+        if (event.key === 'D' || event.key === 'd') {
+            character.style.backgroundImage = 'url("images/personnage/Run.png")';
+            character.classList.add('runAnimation');
+            parallaxContainer.classList.add('parallaxAnimation');
+            parallaxImages.forEach((image) => {
+                image.classList.add('parallaxAnimation');
+            });
+        }
+    }
+
+    document.addEventListener('keyup', stopD);
+    function stopD(event) {
+        if (event.key === 'D' || event.key === 'd') {
+            character.style.backgroundImage = 'url("images/personnage/Run.png")';
+            character.classList.remove('runAnimation');
+            character.style.backgroundImage = 'url("images/personnage/Idle.png")';
+            character.classList.add('IdleAnimation');
+            parallaxContainer.classList.remove('parallaxAnimation');
+            parallaxImages.forEach((image) => {
+                image.classList.remove('parallaxAnimation');
+            });
+        }
+    }
 });
 
 
